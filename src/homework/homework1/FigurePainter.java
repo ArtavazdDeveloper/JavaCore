7cc84eec8958c31a384379e09d6537a9b239b2a8
ArtavazdDeveloper <artavazd1993@gmail.com>
2022-12-20 13:01:57 +0400

    My repository

--- src/homework/homework1/FigurePainter.java
+++ src/homework/homework1/FigurePainter.java
@@ -1,5 +1,31 @@
 package homework.homework1;
 
 public class FigurePainter {
+    public static void main(String[] args) {
 
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int k = 1; k <= 5; k++) {
+            for (int l = 1; l <= 5 - k; l++) {
+                System.out.print("   ");
+            }
+            for (int i = 1; i <= k; i++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int l = 4; l >= 0; l--) {
+            for (int k = 0; k <= l; k++) {
+                System.out.print(" * ");
+
+            }
+            System.out.println();
+        }
+    }
 }
