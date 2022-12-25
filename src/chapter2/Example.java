7cf1ef03c1c09d17e95fd20368bfe9481a8eca89
ArtavazdDeveloper <artavazd1993@gmail.com>
2022-12-25 22:20:33 +0400

    My repository

--- src/chapter2/Example.java
+++ src/chapter2/Example.java
@@ -1,7 +1,7 @@
 package chapter2;
 
 public class Example {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Простая программа на Java.");
     }
 }
