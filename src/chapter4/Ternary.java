60f3424b5efdf3c2ad7a45ef2a19dc676b2a22f7
ArtavazdDeveloper <artavazd1993@gmail.com>
2022-12-27 15:53:24 +0400

    My repository

---
+++ src/chapter4/Ternary.java
@@ -0,0 +1,18 @@
+package chapter4;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int i, k;
+        i = 10;
+        k = i < 0 ? - i : i; // получить обсалютное
+                            // значение переменной i
+        System.out.print(" Абсолютное значение ");
+        System.out.println(i + " равно " + k);
+        i = - 10;
+        k = i < 0 ? - i : i;  // получить абсолютное
+                             // значение переменной i
+        System.out.print("Абсолютное значение  ");
+        System.out.print("Абсолютное значение  ");
+        System.out.println(i + " равно " + k);
+     }
+}
---
+++ src/homework/homework3/ArrayUtil.java
@@ -0,0 +1,52 @@
+package homework.homework3;
+
+public class ArrayUtil {
+    public static void main(String[] args) {
+        int[] array = { 2, 5, 8, 10, 15, 32, 22, 13, 11, 66, 12, 113, 30, 68, 111, 80, 119,  82, 117, 123, 124 };
+        int even = array[0];
+        int odd = array[0];
+        int numbereven = 0;
+        int numberood = 0;
+        int arraysum = 0;
+        int numbrarray = 0;
+        int average = 0;
+        System.out.print("even : ");
+
+        for (int i = 0; i < array.length; i++) {
+            if ((array[i] % 2) == 0) {
+                even = array[i];
+                System.out.print(even + " ");
+            }
+        }
+        System.out.println();
+        System.out.print("odd : ");
+        for (int j = 0; j < array.length; j++) {
+            if ((array[j] % 2) != 0) {
+                odd = array[j];
+                System.out.print(odd + " ");
+            }
+        }
+        System.out.println();
+        for (int k = 0; k < array.length; k++) {
+            if (array[k] % 2 == 0) {
+                numbereven++;
+            } else  {
+                numberood++;
+            }
+        }
+        System.out.println("number of even : " + numbereven );
+        System.out.println("number of odd : " + numberood);
+
+        for (int i = 0; i < array.length; i++) {
+            arraysum = arraysum + array[i];
+        }
+        System.out.println("sum of  array : " + arraysum);
+        average = arraysum / array.length;
+        System.out.println("average : " + average);
+        System.out.println("first elements : " + array[0] );
+        System.out.println("last elements : " + array[20] );
+        System.out.println("last elements : " + array[10] );
+
+
+    }
+}
