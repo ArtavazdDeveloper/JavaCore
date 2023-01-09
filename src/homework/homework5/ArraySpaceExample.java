c1ec7f4c08fd655f83bce79592a3b86b81e225d7
ArtavazdDeveloper <artavazd1993@gmail.com>
2023-01-09 12:59:09 +0400

    My repository

--- src/homework/homework5/ArraySpaceExample.java
+++ src/homework/homework5/ArraySpaceExample.java
@@ -6,30 +6,25 @@ public class ArraySpaceExample {
     public static void main(String[] args) {
 
         char[] spaceArray = {' ', 'c', 'a', 't', ' ', 'b', 'i', ' ', 'b', ' ', ' '};
-        int endSpacesCount = 0;
+        int newArrayLength = 0;
         int frontSpacesCount = 0;
-        boolean stopFront = false;
-        boolean stopEnd = false;
 
         for (int i = 0; i < spaceArray.length; i++) {
-            if (spaceArray[i] == ' ' && !stopFront) {
-
+            if (spaceArray[i] == ' ') {
+                newArrayLength++;
                 frontSpacesCount++;
             } else {
-                stopFront = true;
-            }
-            if (spaceArray[spaceArray.length - 1 - i] == ' ' && !stopEnd) {
-                endSpacesCount++;
-            } else {
-                stopEnd = true;
-            }
-
-            if (stopFront && stopEnd) {
                 break;
             }
         }
-
-        char[] newArray = new char[spaceArray.length - frontSpacesCount - endSpacesCount];
+        for (int i = spaceArray.length - 1; i > 0; i--) {
+            if (spaceArray[i] == ' ') {
+                newArrayLength++;
+            } else {
+                break;
+            }
+        }
+        char[] newArray = new char[spaceArray.length - newArrayLength];
 
         for (int i = 0; i < newArray.length; i++) {
             newArray[i] = spaceArray[i + frontSpacesCount];
