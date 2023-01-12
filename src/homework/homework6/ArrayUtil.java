edb9965b521209364876b2bfc1824bf240e91415
ArtavazdDeveloper <artavazd1993@gmail.com>
2023-01-12 04:03:42 +0400

    My repository

---
+++ src/homework/homework6/ArrayUtil.java
@@ -0,0 +1,87 @@
+package homework.homework6;
+
+public class ArrayUtil {
+    void minOfArray(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+
+
+            }
+
+        }
+        System.out.println("min:" + min);
+    }
+
+    void maxOfArray(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+
+            }
+
+        }
+        System.out.println("max:" + max);
+    }
+
+    void evenOfArray(int[] array) {
+        System.out.print("even: ");
+        for (int i = 0; i < array.length; i++) {
+            if ((array[i] % 2) == 0) {
+                int even = array[i];
+                System.out.print(even + " ");
+            }
+
+        }
+        System.out.println();
+    }
+
+    void oddOfArray(int[] array) {
+
+        System.out.print("odd: ");
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] % 2 != 0) {
+                int odd = array[j];
+                System.out.print(odd + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    void quantityOfArray(int[] array) {
+        int numbereven = 0;
+        int numberood = 0;
+        for (int k = 0; k < array.length; k++) {
+            if (array[k] % 2 == 0) {
+                numbereven++;
+            } else {
+                numberood++;
+            }
+        }
+        System.out.println("number of even: " + numbereven);
+        System.out.println("number of odd: " + numberood);
+    }
+
+    void sumOfArray(int[] array) {
+        int arraysum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            arraysum = arraysum + array[i];
+
+        }
+        System.out.println("sum of  array: " + arraysum);
+    }
+
+    void averageOfArray(int[] array) {
+        int arraysum = 0;
+        for (int i = 0; i < array.length; i++) {
+            arraysum = arraysum + array[i];
+
+        }
+        int average = arraysum / array.length;
+        System.out.println("average: " + average);
+    }
+
+}
---
+++ src/homework/homework6/ArrayUtilDemo.java
@@ -0,0 +1,19 @@
+package homework.homework6;
+
+public class ArrayUtilDemo {
+    public static void main(String[] args) {
+
+        int[] array = {2, 5, 8, 10, 88, 22, 11, 66, 12, 30, 6, 4, 3};
+        ArrayUtil ar = new ArrayUtil();
+        ar.minOfArray(array);
+        ar.maxOfArray(array);
+        ar.evenOfArray(array);
+        ar.oddOfArray(array);
+        ar.quantityOfArray(array);
+        ar.sumOfArray(array);
+        ar.averageOfArray(array);
+
+
+
+    }
+}
